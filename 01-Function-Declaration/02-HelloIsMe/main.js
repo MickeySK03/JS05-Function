61ac92f4e6a2a9b5b01e6786d24e0d0a264ebd0e
mixsorawit@gmail.com <mixsorawit@gmail.com>
2023-08-15 21:19:53 +0700

    02HelloEdit

--- 01-Function-Declaration/02-HelloIsMe/main.js
+++ 01-Function-Declaration/02-HelloIsMe/main.js
@@ -1,17 +1,14 @@
-<<<<<<< HEAD
+
 let userName = prompt("enter a user");
 
 function sayHelloUser() {
     alert(`Hello ${userName}`);    
 }
 
-sayHelloUser(userName);
-=======
+
+
 let user = prompt("Enter your name")
 
 function sayHelloUser(){
     alert(`Hello ${user}`)
 }
-
-sayHelloUser()
->>>>>>> refs/remotes/origin/main
